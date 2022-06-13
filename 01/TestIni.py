3c6203671b7537e53628d5a428437a4426eefc16
fx202101 <fx202101@gmail.com>
2022-06-13 12:34:04 +0900

    test

--- 01/TestIni.py
+++ 01/TestIni.py
@@ -1,7 +1,7 @@
 import configparser
 import os
 
-#相対パスの取得できなため、OSコマンドを使ってうまくいけます
+# 相対パスの取得できなため、OSコマンドを使ってうまくいけます
 
 config = configparser.ConfigParser()
 path = os.path.join(os.path.dirname(__file__), 'test.ini')
@@ -9,4 +9,3 @@ config.read(path)
 
 print(config.sections())
 print(config['TEST']['TEST_KEY1'])
-
